f1695929967ec32745837552982bbe9b43bcb4b9
Yijunhyeong <ljh040325@gmail.com>
2024-01-12 15:35:29 +0900

    .

--- src/main/java/com/example/demo12131/controller/MainController.java
+++ src/main/java/com/example/demo12131/controller/MainController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Objects;
 
 @Controller
-@CrossOrigin(origins = "https://www.springbootproject.com/")
+@CrossOrigin(origins = "https://www.springbootproject.com")
 public class MainController {
     PostDAO pd = new PostDAO();
 
